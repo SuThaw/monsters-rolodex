fd74e0e8ee7afc2ac91c650ec5fb66b80024afab
Thaw Lwin Htun <thaw.htun@singtel.com>
2020-01-03 17:47:39 +0800

    Updates

---
+++ precache-manifest.b59b32b6ed0334bd7fd71f8ffffab0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9282996cb5da234ae02a04d5ba926dc6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3ef78b5bc3f29374f4f8",
+    "url": "/monsters-rolodex/static/css/main.74804a33.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ef78b5bc3f29374f4f8",
+    "url": "/monsters-rolodex/static/js/main.127ff06f.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
